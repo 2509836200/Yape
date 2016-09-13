9edb7eaba22761775cd7ef5ca31e620bb7b8dca3
李霞 <lixia@lixiadeiMac.local>
2016-09-13 18:55:09 +0800

    yape首页

---
+++ MFB/Yape/View/cells/YapeCell.h
@@ -0,0 +1,13 @@
+//
+//  YapeCell.h
+//  MFB
+//
+//  Created by 李霞 on 16/9/13.
+//  Copyright © 2016年 xinpingTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YapeCell : UITableViewCell
+-(void)SetContentWithDictionary:(NSDictionary *)contentDic;
+@end
