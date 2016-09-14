b6955822a1c5c4d442f02cb4f7e10cc6cd1bbad6
李霞 <lixia@lixiadeiMac.local>
2016-09-14 10:29:10 +0800

    创建 IssuedDatingViewController

---
+++ MFB/Yape/YapeController/IssuedDatingViewController.h
@@ -0,0 +1,13 @@
+//
+//  IssuedDatingViewController.h
+//  MFB
+//
+//  Created by 李霞 on 16/9/14.
+//  Copyright © 2016年 xinpingTech. All rights reserved.
+//
+
+#import "BaseViewController.h"
+//发布邀约Controller
+@interface IssuedDatingViewController : BaseViewController
+
+@end
